a7a6348dcff24120dce8cd0b16048f87fd7a3afa
Agustín Souto <69278386+Souto751@users.noreply.github.com>
2022-09-27 22:03:23 -0300

    Add files via upload

---
+++ logueo/develop/services/authEncrypt.php
@@ -0,0 +1,33 @@
+<?php
+
+    require_once("../utils/connection.php");
+
+    try{
+        $mysql = new connection();
+        $con = $mysql->get_connection();
+
+        $user = $_GET["user"];
+        $pass = $_GET["pass"];
+
+        $statement = $con->prepare("CALL authencrypted(?, ?)");
+        $statement->bind_param("ss", $user, $pass);
+        $statement->execute();
+        $result = $statement->get_result();
+        $array = array();
+
+        if($con->affected_rows > 0){
+            while($data = $result->fetch_assoc()){
+                array_push($array, $data);
+            }
+            echo json_encode(array('status' => 'ok', 'data' => $array));
+            
+        }else{
+            echo json_encode(array('status' => 'error', 'message' => 'Invalid user/password!'));
+        }
+        
+        $statement->close();
+        $con->close();
+    }catch(Exception $e){
+        echo json_encode(array('status' => 'error', 'message' => $e->getMessage()));
+    }
+?>
